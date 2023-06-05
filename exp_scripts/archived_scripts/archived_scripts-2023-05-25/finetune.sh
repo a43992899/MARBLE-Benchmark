fc04eef884696930d603e6d99f2774c386eeaf50
a43992899 <a43992899@163.com>
2023-06-05 19:40:33 -0400

    remove old scripts

--- exp_scripts/archived_scripts/archived_scripts-2023-05-25/finetune.sh
+++
@@ -1,8 +0,0 @@
-# TODO: finish header for this file, and update readme
-
-python . finetune   --finetune False 
-                    --dataset GTZAN --audio_dir data/GTZAN/genres --batch_size 4 --monitor valid_acc \
-                    --hblayer None --hbreduction mean --probelayer all \
-                    --pre_trained_folder ./data/hubert_data/HF_HuBERT_base_MPD_train_1000h_valid_300h_iter2_400k_vanilla_model_ncluster_2000_ckpt_54_200000
-                    --sample_duration 30
-
--- exp_scripts/archived_scripts/archived_scripts-2023-05-25/sync_offline_wandb.sh
+++
@@ -1,9 +0,0 @@
-__conda_setup="$('/home/yrb/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)" ;  eval "$__conda_setup"
-conda activate map 
-
-while true
-do
-    echo "waiting for 5 minutes to sync"
-    sleep 5m
-    wandb sync /home/yizhi/baai_wandb/wandb/run*
-done
