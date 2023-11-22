a12ad1e3f486f0276c88e85f25a62393b32ff336
a43992899 <a43992899@163.com>
2023-11-22 03:23:51 -0500

    update fewshot scripts

--- exp_scripts/fewshot/fewshot_MERT-v1-330M.sh
+++ exp_scripts/fewshot/fewshot_MERT-v1-330M.sh
@@ -3,7 +3,7 @@ TASKS=(GTZAN GS VocalSetT VocalSetS)
 for ((i=0; i<${#TASKS[@]}; i++)); do
     for LAYER in all $(seq 24 -1 0)
     do
-        python . probe -c configs/mert/MERT-v1-330M/${TASKS[i]}.yaml \
+        python . fewshot -c configs/mert/MERT-v1-330M/${TASKS[i]}.yaml \
         -o "model.downstream_structure.components[0].layer='${LAYER}'"
     done
 done
--- exp_scripts/fewshot/fewshot_others.sh
+++ exp_scripts/fewshot/fewshot_others.sh
@@ -3,7 +3,7 @@ MODELS=(clmr encodec jukemir mule musicnn)
 
 for ((i=0; i<${#TASKS[@]}; i++)); do
     for ((j=0; j<${#MODELS[@]}; j++)); do
-            python . probe -c configs/${MODELS[j]}/${TASKS[i]}.yaml \
+            python . fewshot -c configs/${MODELS[j]}/${TASKS[i]}.yaml \
             -o "model.downstream_structure.components[0].layer='${LAYER}'"
     done
 done
